81d17e8ec23e70484504ee3f7784f566bd26709c
Daria Dorinina <doronindaria@gmail.com>
2022-09-24 19:19:11 +0300

    some comments

--- main.c
+++
@@ -1,7 +0,0 @@
-#include "print.h"
-#include <stdio.h>
-
-int main(void) {
-    print_fio();
-    return 0;
-}
--- print.c
+++
@@ -1,5 +0,0 @@
-#include "print.h"
-#include <stdio.h>
-void print_fio(void) {
-    printf("Доронина Дарья Юрьевна\nВИ921Б\n");
-}
